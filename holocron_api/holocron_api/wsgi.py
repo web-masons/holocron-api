df061d9e88cf9fc5be7483c01a4699c0668995a6
Holly Fletcher <hfletcher@carbonite.com>
2015-04-28 18:34:50 -0400

    - Updated GitIgnore - Added static folder for Django REST - setup Apache - Apache :80 pointed to Django with REST assets

--- holocron_api/holocron_api/wsgi.py
+++ holocron_api/holocron_api/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+#from whitenoise.django import DjangoWhiteNoise
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/vagrant/holocron_api')
+sys.path.append('/vagrant/holocron_api/holocron-api')
+sys.path.append('/vagrant/holocron_api/api')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "holocron_api.settings")
 
 application = get_wsgi_application()
+#application = DjangoWhiteNoise(application)
