7bf12d8971d0a03105c189c22f01d7b8fe9ac583
Asa Gage <asagage@gmail.com>
2015-05-13 12:00:02 -0400

    updated path for static files

--- holocron_api/holocron_api/settings/local.py
+++ holocron_api/holocron_api/settings/local.py
@@ -6,10 +6,10 @@ SECRET_KEY = 'NOT_SO_SECRET_KEY'
 
 DATABASES = {
     'default': {
-        'ENGINE': django.db.backends.postgresql_psycopg2,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'holocron',
         'USER': 'django-test',
-        'PASSWORD': 'testing'
+        'PASSWORD': 'testing',
         'HOST': 'localhost'
     }
 }
