aa55e4bee8bb28087bb031060427188ff17d41ea
Holly Fletcher <hfletcher@carbonite.com>
2015-06-17 14:45:07 -0400

    Travis comment updates

--- holocron_api/api/models/__init__.py
+++ holocron_api/api/models/__init__.py
@@ -1,8 +1,7 @@
-#Importing here prevents needing to import each model into other files, such as views
+# Importing here prevents needing to import each model into other files
 
-from .LOB import *  #noqa
-from .Audience import * #noqa
-from .Intent import * #noqa
-from .Lifecycle import * #noqa
-from .standard import * #noqa
-
+from .LOB import *  # noqa
+from .Audience import * # noqa
+from .Intent import * # noqa
+from .Lifecycle import * # noqa
+from .standard import * # noqa
