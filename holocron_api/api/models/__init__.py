c1df4e2f7e40d4e9a83ce0020541ef8ecceb5d10
Holly Fletcher <hfletcher@carbonite.com>
2015-06-17 14:49:44 -0400

    more spaces for Travis

--- holocron_api/api/models/__init__.py
+++ holocron_api/api/models/__init__.py
@@ -1,7 +1,7 @@
 # Importing here prevents needing to import each model into other files
 
 from .LOB import *  # noqa
-from .Audience import * # noqa
-from .Intent import * # noqa
-from .Lifecycle import * # noqa
-from .standard import * # noqa
+from .Audience import *  # noqa
+from .Intent import *  # noqa
+from .Lifecycle import *  # noqa
+from .standard import *  # noqa
