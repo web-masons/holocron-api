d03d3a4b09c731f2e6fe7d9210a168328414f616
Holly Fletcher <hfletcher@carbonite.com>
2015-07-09 17:00:14 -0400

    Updates to fix travis extended max line length

--- holocron_api/blacklist_manager/views.py
+++ holocron_api/blacklist_manager/views.py
@@ -1,7 +1,7 @@
 from blacklist_manager.models import *  # noqa
 from rest_framework import viewsets
 from blacklist_manager.serializers import *  # noqa
-# Create your views here.
+
 
 class BlacklistEntryViewSet(viewsets.ModelViewSet):
     """
@@ -9,6 +9,7 @@ class BlacklistEntryViewSet(viewsets.ModelViewSet):
     """
     queryset = BlacklistEntry.objects.all()
     serializer_class = BlacklistEntrySerializer
+
 
 class EntryViewSet(viewsets.ModelViewSet):
     """
