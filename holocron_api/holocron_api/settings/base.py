9233a4376394057cc225d695208a54c02d6edc0d
Asa Gage <asagage@gmail.com>
2015-11-25 16:27:56 -0500

    adding support for page_size in requests

--- holocron_api/holocron_api/settings/base.py
+++ holocron_api/holocron_api/settings/base.py
@@ -33,7 +33,9 @@ INSTALLED_APPS = (
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',)
-    'PAGE_SIZE': 10,
+    # 'PAGE_SIZE': 10,
+    'PAGINATE_BY': 10,
+    'PAGINATE_BY_PARAM': 'page_size',
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
 }
 
